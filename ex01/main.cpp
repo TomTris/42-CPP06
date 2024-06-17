a4c4b09ccf43bb9b3e08e752596e0f716ae367ee
Quoc Tri Do <qdo@1-B-25.42heilbronn.de>
2024-06-17 09:37:46 +0200

    add check sizeof, convert decimalto hex

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,13 +6,23 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 19:25:02 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/16 20:11:05 by qdo              ###   ########.fr       */
+/*   Updated: 2024/06/17 09:36:29 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
 #include "Serialization.hpp"
+#include <sstream>
+
+
+std::string decimalToHex(unsigned long long nbr)
+{
+     std::stringstream ss;    
+
+     ss<< std::hex << nbr;
+     return ss.str();
+}
 
 int	main(void)
 {
@@ -33,18 +43,20 @@ int	main(void)
 
     std::cout << "OG foo (Data) Address = " << &foo << std::endl
          << "str value = " << foo.str << std::endl;
-
     std::cout << std::endl;
-
     std::cout << "Reinterpreted foo value = " << reinterpreted << std::endl;
-
     std::cout << std::endl;
-
     Data *deserialized;
-
     deserialized = Serialization::deserialize(reinterpreted);
 
     std::cout << "Deserialized Address = " << deserialized << std::endl
          << "str value = " << deserialized->str << std::endl;
+     std::cout << sizeof(&foo) << " : " << sizeof(deserialized) << std::endl;
+
+     std::cout << " ----------- ------- " << std::endl;
+     unsigned long long decimalNumber = 140732754241344;
+     std::string hexNumber = decimalToHex(decimalNumber);
+     std::cout << "The hex: " << hexNumber << std::endl;
+     std::cout << "0x7ffee5d32740" << std::endl;
 
 }
